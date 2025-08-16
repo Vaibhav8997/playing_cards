1fcfd30a6ccf20da262c884409b5c011944814a0
Vaibhav Singh <singhrisu2000@gmail.com>
2025-08-16 13:54:35 +0530

    Adedd new func newDeckFromFile with error handling

--- main.go
+++ main.go
@@ -2,20 +2,8 @@ package main
 
 // import "fmt"
 func main() {
-	// //var card string = "Ace of spades"
-	// card := newCard() //colun equal to operator
-	// fmt.Println(card)
-
-	//slice
-	// cards := newDeck()
-
-	// //cards.print()
-	// hand, remaningCards := deal(cards, 5)
-
-	// hand.print()
-	// remaningCards.print()
-
-	cards := newDeck()
-	cards.saveToFile("my_caards")
+	cards := newDeckFromFile("my_caards")
+	// cards := newDeckFromFile("my") :To check error handling
+	cards.print()
 
 }
